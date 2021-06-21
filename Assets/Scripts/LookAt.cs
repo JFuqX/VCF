d72113b90ee52cde8627f57ab87e7676a31d3126
Nico Merkel <nico.merkel1999@gmx.de>
2021-06-21 10:04:53 +0200

    Added UI and Character

---
+++ Assets/Scripts/LookAt.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, speed * Time.deltaTime);
+
+
+    }
+}
